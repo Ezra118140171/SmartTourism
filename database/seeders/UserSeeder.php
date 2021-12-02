2dc0a8feabfd07760e417f0d8d2661f20dce840e
Ezra118140171 <ezra.118140171@student.itera.ac.id>
2021-12-02 18:37:31 +0700

    tabel kendaraan

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use PhpParser\Node\Stmt\Foreach_;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            [
+                'name'      => 'admin',
+                'email'     => 's@s',
+                'password'  => bcrypt(1),
+            ],
+        ];
+        foreach ($user as $key => $value) {
+            User::create($value);
+        }
+    }
+}
